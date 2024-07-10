e6da5a0ddac557b583e93ab4b398c226b25f8f06
kalanakt <e19198@eng.pdn.ac.lk>
2024-07-10 20:05:38 +0530

    fx flutter typo

--- example/main.dart
+++ example/main.dart
@@ -1,6 +1,9 @@
+import 'dart:developer';
 import 'package:flutter_i18n_converter/flutter_i18n_converter.dart';
 
 void main() {
   const String shortCode = 'en';
   String longCode = I18nConverter.getLanguageName(shortCode); // Output: English
+
+  log(longCode);
 }
