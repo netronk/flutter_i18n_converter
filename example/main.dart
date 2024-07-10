9cc083690e7f62b64d56ccc4b08523107815baa8
kalanakt <e19198@eng.pdn.ac.lk>
2024-07-10 20:01:09 +0530

    adde example and fix pub publish

---
+++ example/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_i18n_converter/flutter_i18n_converter.dart';
+
+void main() {
+  const String shortCode = 'en';
+  String longCode = I18nConverter.getLanguageName(shortCode); // Output: English
+}
